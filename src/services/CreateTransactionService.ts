543eb99e1c4ca24c866827cf2e4a5eb38703bbd0
tiagobani <tiago.bani@yahoo.com.br>
2020-09-02 05:53:32 -0300

    [feat]: remove unused import

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,7 +1,6 @@
 import AppError from '../errors/AppError';
 import Transaction from '../models/Transaction';
 import Category from '../models/Category';
-import TransactionsRepository from '../repositories/TransactionsRepository';
 
 interface CreateTransactionDTO {
   title: string;
